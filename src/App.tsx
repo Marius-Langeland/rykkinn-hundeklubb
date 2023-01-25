e850b16995d786b052339a46bdf371568d31d2eb
Marius <mariuslangeland.ml@gmail.com>
2023-01-26 00:32:17 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.scss';
 import './theme.css';
-import Dog from "./images/Dog_Calm.png"
+import Dog from "./images/Dog_profile_2.png"
 import {ReactComponent as Bridge} from "./images/Untitled.svg";
 
 function App() {
   return (
     <>
+      <div id="background"></div>
       <h1><span>Rykkinn</span> <span>hunde-</span> <span>klubb</span></h1>
       <img id='dog-sideview' src={Dog} alt="" />
-      <Bridge id='bridge'/>
-      <div id="follow"></div>
+      <section id="content">
+        <Bridge id='bridge'/>
+        <div id="main"></div>
+      </section>
     </>
   );
 }
